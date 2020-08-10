f2af99dfd186805254ce0f27310f59a82c92d9b3
tylergreason <tyler.greason@protonmail.com>
2020-08-10 10:11:39 -0400

    Made module, had class inherit its method, another class subclassed that class, tested default values and more and they all work

--- practice.rb
+++ practice.rb
@@ -1,18 +1,46 @@
-p "this is practice!"
+# create a module for classes and their instances to inherit 
+module MyModule 
+    def myModuleMethod
+        p "This is a method attached to my module"
+    end
+end
+
 
 # play with classes 
 class Animal 
+    # attach the methods and constants in MyModule to this class
+    include MyModule
     # have to manually set getters, setters, or both 
     attr_reader :species, :color
     attr_writer :color
     attr_accessor :name
     # initialize with variables taken as arguments 
-    def initialize (species = "None")
+    def initialize (species = "None", color="blue")
         @species = species
-        # or set default values here: 
-        @color = "blue"
+        @color = color
         # can fire methods when the instance is created 
         p @species
+    end
+end
+
+# make another class that inherits from Animal 
+class Mammal < Animal
+    attr_accessor :fur 
+    def initialize(fur=false)
+        @fur = fur
+    end 
+
+    def walk 
+        p "This animal is walking"
+    end
+
+    def hasFur?
+        if self.fur
+            has = "does"
+        else
+            has = "does not"
+        end
+        p "This animal #{has} have fur"
     end
 end
 
@@ -25,3 +53,15 @@ p cat.color
 # accessor; 
 cat.name = 'Jerry' 
 p cat.name
+
+human = Mammal.new(true)
+human.walk
+human.hasFur?
+whale = Mammal.new 
+whale.hasFur?
+# call method of module attached to this instance's class's parent class
+# also, ruby doesn't require paranthesis to call function but it's a good habit to keep 
+human.myModuleMethod
+
+
+cat.myModuleMethod()
